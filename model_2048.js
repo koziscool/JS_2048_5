3aa3d384686f989b2185b2af5979bd60c86c8a5e
John Kosmicke <kozjob@yahoo.com>
2019-04-22 17:51:53 -0700

    update

--- model_2048.js
+++ model_2048.js
@@ -1,30 +1,56 @@
 
 
 model2048 = {
-  size: 4,
+  side: 4,
   numSquares: 16,
 
   values: [],
 
   init: function(  ) {
-     for( var i = 0; i < this.numSquares ; i++ ) {
-       this.values[i] = 0;
-     }
+    for( var i = 0; i < this.numSquares ; i++ ) {
+      this.values[i] = 0;
+    }
+    this.insertRandomTile();
+    this.insertRandomTile();
+  },
+
+  insertRandomTile: function(  ) {
+    var zeroes = [];
+    for( var i = 0; i < this.values.length ; i++ ) {
+      if( this.values[i] === 0 ) zeroes.push(i);
+    }
+    var rand = Math.floor( Math.random() * zeroes.length );
+    var newValue = Math.random() < 0.9 ? 2 : 4;
+    this.values[rand] = newValue;
   },
 
   updateGrid: function( direction ) {
     
   },
 
+  collapseRow: function( arr ) {
+    var retArr = [];
+    return retArr;
+  },
+
 
   getRow: function( row ) {
-    
+    return  [
+      this.values[row * this.side ],
+      this.values[row * this.side + 1 ],
+      this.values[row * this.side + 2 ],
+      this.values[row * this.side + 3]
+    ];
   },
 
   getCol: function( col ) {
-    
+    return  [
+      this.values[col],
+      this.values[col + this.side * 1 ],
+      this.values[col + this.side * 2 ],
+      this.values[col + this.side * 3]
+    ];
   },
-
 };
 
 
--- view.js
+++ view.js
@@ -5,12 +5,23 @@ view2048 = {
   model: model2048,
 
   init: function(  ) {
+    this.update();
+    this.addClickHandler();
+  },
+
+  update: function(  ) {
     this.$grid = $("#grid-2048");
     for( var i = 0; i < this.model.numSquares ; i++ ) {
-      var $square = $("<div><div class='value'>" + this.model.values[i] + "</div></div>");
+      var value = this.model.values[i];
+      //var value = this.model.values[i] || "";
+      var $square = $("<div><div class='value'>" + value + "</div></div>");
       $square.addClass("square");
       this.$grid.append($square);
     }
+  },
+
+  addClickHandler: function( ) {
+    
   },
 }
 
